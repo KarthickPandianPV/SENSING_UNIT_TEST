2d1eba887f0d442f371242ac48292b724c5c3cec
Karthick <karthickkp2005@gmail.com>
2023-12-18 18:52:24 +0530

    Initial Commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/karthick/Documents/PlatformIO/Projects/MPU_test/src
+# Build directory: /home/karthick/Documents/PlatformIO/Projects/MPU_test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
